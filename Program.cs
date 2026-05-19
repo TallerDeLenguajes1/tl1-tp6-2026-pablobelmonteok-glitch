afee4dd8e4fcc3bb7be3ec922b090729af96e831
“pablobelmonteok-fallo” <pablobelmontecrack@gmail.com>
2026-05-19 13:29:53 -0300

    primer commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+Console.WriteLine("Hello, World!");
+int a ; 
+int b ;
+
+a = 10;
+b=a;
+Console.WriteLine("Valor de a:"+a);
+Console.WriteLine("Valor de b:"+b);
+
+int numero = 10;
+if (numero>0)
+{
+    numero = numero *(-1);
+};
+Console.WriteLine("Valor de numero:"+numero);
+
+int numero1,numero2 = 0;
